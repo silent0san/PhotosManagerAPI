6f47135c8424520a341992aa27a377db0c1ea2d1
Gabriel Kurzak <gabrielkurzak@gmail.com>
2022-10-25 17:12:52 +0200

    Created 'Photo' model, base serializer, API views

--- PhotosManagerAPI/settings.py
+++ PhotosManagerAPI/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'rest_framework',
+    'django_filters',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -130,4 +131,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
 }
